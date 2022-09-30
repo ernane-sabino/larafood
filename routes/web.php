34a14960e8861ada5916fe314babd979364d4cd8
Ernane Sabino <esabino@stefanini.com>
2022-09-30 07:43:45 -0300

    Delete plans

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PlanController;
 
+Route::delete('admin/plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
 Route::get('admin/plans/{url}', [PlanController::class, 'show'])->name('plans.show');
 Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
 Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
